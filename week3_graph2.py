5efc7273d394438c05c7e73bfc48d0d9e6188fc0
Eunkyy <melon0802@naver.com>
2024-03-27 23:43:38 +0900

    :sparkles: 데이터분석 실습3

---
+++ week3_graph2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('/content/drive/MyDrive/colab/deeplearning/dataset/pima-indians-diabetes.csv',
+                 names = ["pregnant", "plasma", "pressure", "thickness", "insulin", "BMI", "pedigree", "age", "class"])
+#print(df.describe().age)
+aver = df.describe().age[1]
+print('Average age:', round(aver, 2))
+data = df[['age']].values
+low_age = df[df['age'] <= aver].thickness
+high_age = df[df['age'] >= aver].thickness
+low_age.plot.hist(bins=100, alpha=0.5)
+high_age.plot.hist(bins=100, alpha=0.5)
+plt.legend(['low_age', 'high_age'])
+plt.show()
