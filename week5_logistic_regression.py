dad304c0ebaba15294be2e7606f0876d925a8b88
eunkyung <88434735+euunrud@users.noreply.github.com>
2024-04-22 02:18:53 +0900

    :recycle: refactor : week5_logistic_regression.py

--- week5_logistic_regression.py
+++ week5_logistic_regression.py
@@ -3,12 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #실습 4_1
-#공부시간 X와 성적 Y의 리스트를 만듭니다.
+#공부시간 X와 성적 Y의 리스트
 data = [[2.0, 0.0], [4.0, 0.0], [6.0, 0.0], [8.0, 1.0], [10.0, 1.0], [12.0, 1.0], [14.0, 1.0]]
 a = 0
 b = 0
 
-#학습률을 정합니다.
+#학습률
 lr = 0.05
 
 x = [i[0] for i in data]
@@ -18,14 +18,14 @@ x_data = np.array(x)
 y_data = np.array(y)
 loss = []
 
-#시그모이드 함수를 정의합니다.
+#시그모이드 함수를 정의
 def sigmoid(x):
     return 1 / (1 + np.e ** (-x))
 
 def error_sum():
   return ((y_data - sigmoid(a*x_data + b))**2).mean()
 
-#경사 하강법을 실행합니다.
+#경사 하강법을 실행
 for i in range(10001):
       a_diff = (1/len(x_data))*sum(x_data *(sigmoid(a*x_data + b) - y_data))
       b_diff = (1/len(x_data))*sum(sigmoid(a*x_data+b)-y_data)
@@ -34,11 +34,11 @@ for i in range(10001):
 
       loss.append(error_sum())
 
-      if i % 1000 == 0:    # 1000번 반복될 때마다 각 x_data값에 대한 현재의 a값, b값을 출력합니다.
+      if i % 1000 == 0:    # 1000번 반복될 때마다 각 x_data값에 대한 현재의 a값, b값을 출력
           print('epoch=%.f. 기울기=%.04f. 절편=%.04f' % (i, a, b))
           print('Error value: %.04f' % loss[i])
 
-# 앞서 구한 기울기와 절편을 이용해 그래프
+# 기울기와 절편을 이용해 그래프
 x_range = (np.arange(0, 10001, 1)) # 그래프로 나타낼 x 범위
 plt.plot(np.arange(0, 10001, 1), loss)
 plt.show()
